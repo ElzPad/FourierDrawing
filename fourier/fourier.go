71664d5fcaaf6aeca483022e72b3002e73356ac5
ElzPad <elziario.paduano@gmail.com>
2024-12-15 22:30:59 +0100

    Modified DiscretFourierTransform to return []complex128

--- fourier/fourier.go
+++ fourier/fourier.go
@@ -4,33 +4,15 @@ import (
 	"math"
 )
 
-type FourierElem struct {
-	Re			float64
-	Im			float64
-	Freq		float64
-	Ampl		float64
-	Phase		float64
-}
-
-func DiscreteFourierTransform(x []float64) ([]FourierElem) {
+func DiscreteFourierTransform(x []float64) ([]complex128) {
 	N := len(x)
-	X := make([]FourierElem, N)
+	X := make([]complex128, N)
 
 	for k:=0; k<N; k++ {
-		re := 0.0
-		im := 0.0
-
 		for n:=0; n<N; n++ {
 			arg := 2 * math.Pi * float64(n) * float64(k) / float64(N);
-			re += x[n] * math.Cos(arg)
-			im -= x[n] * math.Sin(arg)
+			X[k] += complex(x[n], 0) * complex(math.Cos(arg), -math.Sin(arg))
 		}
-
-		freq := float64(k)
-		ampl := math.Sqrt(re*re + im*im)
-		phase := math.Atan2(im, re)
-
-		X[k] = FourierElem{re, im, freq, ampl, phase}
 	}
 
 	return X
