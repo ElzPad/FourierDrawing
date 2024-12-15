57b83086c1b32862179d09f24b637e91d7c8bd06
ElzPad <elziario.paduano@gmail.com>
2024-12-15 22:44:48 +0100

    Implemented InverseDFT

--- fourier/fourier.go
+++ fourier/fourier.go
@@ -17,3 +17,20 @@ func DiscreteFourierTransform(x []float64) ([]complex128) {
 
 	return X
 }
+
+func InverseDFT(X []complex128) ([]float64	) {
+	N := len(X)
+	x := make([]float64, N)
+
+	for n:=0; n<N; n++ {
+		res := 0.0+0.0i
+
+		for k:=0; k<N; k++ {
+			arg := 2 * math.Pi * float64(n) * float64(k) / float64(N);
+			res += X[k] * complex(math.Cos(arg), math.Sin(arg))
+		}
+		x[n] = real(res)/float64(N)
+	}
+
+	return x
+}
