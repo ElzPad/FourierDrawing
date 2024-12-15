e8ea4926fbe70aa6b500e35efc19728740b0b0d5
ElzPad <elziario.paduano@gmail.com>
2024-12-15 17:33:10 +0100

    Implemented drawEmptyCircle function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -18,6 +19,7 @@ const (
 	End
 )
 
+// Required from Ebiten.
 // Game implements ebiten.Game interface.
 type Game struct {
 	windowSize  struct{ width, height int }
@@ -26,6 +28,7 @@ type Game struct {
 	revealIndex int
 }
 
+// Required from Ebiten.
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
@@ -57,6 +60,21 @@ func (g *Game) Update() error {
 	return nil
 }
 
+func drawEmptyCircle(screen *ebiten.Image, cx, cy, r float64, lineColor color.Color) {
+	steps := 20
+	dAngle := 2*math.Pi/float64(steps)
+
+	point1 := struct{ x,y float64 }{cx+r, cy}
+	point2 := struct{ x,y float64 }{0, 0}
+	for i:=1; i<=steps; i++ {
+		point2.x = cx+r*math.Cos(dAngle*float64(i))
+		point2.y = cy+r*math.Sin(dAngle*float64(i))
+		ebitenutil.DrawLine(screen, point1.x, point1.y, point2.x, point2.y, lineColor)
+		point1 = point2
+	}
+}
+
+// Required from Ebiten.
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -74,9 +92,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	for i:=1; i<upperLimit; i++ {
 		ebitenutil.DrawLine(screen, g.points[i-1].x, g.points[i-1].y, g.points[i].x, g.points[i].y, lineColor)
+		drawEmptyCircle(screen, g.points[i].x, g.points[i].y, 10, lineColor)
 	}
 }
 
+// Required from Ebiten.
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
